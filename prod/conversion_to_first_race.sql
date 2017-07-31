b5da00f8b1fcb38a749733aff03088f70401d8c5
Eperjesi <Vladimir.Eperjesi@adastragrp.com>
2017-07-31 13:32:57 +0200

    mviews

---
+++ prod/conversion_to_first_race.sql
@@ -0,0 +1,63 @@
+SELECT
+  uid,
+  country,
+  app_version,
+  device_category,
+  mobile_brand_name,
+  mobile_model_name,
+  mobile_marketing_name,
+  event_name,
+  platform,
+  event_timestamp_ms,
+  event_timestamp,
+  user_creation_timestamp,
+  days_retained,
+  ltv_revenue,
+  up_first_app_version,
+  up_gems,
+  up_coins,
+  up_track_level,
+  up_first_open_time,
+  up_iapPurchasedCount,
+  up_track_level_backend,
+  up_rank,
+  up_user_property_rank,
+  ep_boss,
+  ep_coins,
+  ep_cup_id,
+  ep_current_level_center_of_mass,
+  ep_current_level_downforce,
+  ep_current_level_engine,
+  ep_current_level_fuel_tank,
+  ep_current_level_gearbox,
+  ep_current_level_grip,
+  ep_current_level_power_distribution,
+  ep_current_level_rollcage,
+  ep_current_level_springs,
+  ep_current_level_turbo,
+  ep_event,
+  ep_firebase_event_origin,
+  ep_firebase_screen_class,
+  ep_firebase_screen_id,
+  ep_gems,
+  ep_open_special,
+  ep_session_coins,
+  ep_session_gems,
+  ep_session_time,
+  ep_special,
+  ep_total_coins,
+  ep_total_gems,
+  ep_total_play_time,
+  ep_total_time,
+  ep_vehicle,
+  ep_wc_rank
+FROM
+  [hill-climb-racing-2-67064304:materialized_views.events_flat_20170702]
+WHERE
+  (event_name = 'tutorial_pedal_pressed'
+    OR event_name = 'cup_0_started')
+  AND uid NOT IN (
+  SELECT
+    uid
+  FROM
+    [hill-climb-racing-2-67064304:materialized_views.lifetime_cheaters_hackers])
---
+++ prod/vehicle_purchased.sql
@@ -0,0 +1,48 @@
+SELECT
+  uid,
+  country,
+  app_version,
+  device_category,
+  mobile_brand_name,
+  mobile_model_name,
+  mobile_marketing_name,
+  event_name,
+  platform,
+  event_timestamp_ms,
+  event_timestamp,
+  user_creation_timestamp,
+  days_retained,
+  ltv_revenue,
+  up_first_app_version,
+  up_gems,
+  up_coins,
+  up_track_level,
+  up_first_open_time,
+  up_iapPurchasedCount,
+  up_track_level_backend,
+  up_rank,
+  up_user_property_rank,
+  ep_coins,
+  ep_cost,
+  ep_firebase_event_origin,
+  ep_firebase_screen_class,
+  ep_firebase_screen_id,
+  ep_gems,
+  ep_session_coins,
+  ep_session_gems,
+  ep_session_time,
+  ep_total_coins,
+  ep_total_gems,
+  ep_total_play_time,
+  ep_total_time,
+  ep_vehicle,
+  ep_wc_rank
+FROM
+  [hill-climb-racing-2-67064304:materialized_views.events_flat_20170702]
+WHERE
+  (event_name = 'vehicle_purchased')
+  AND uid NOT IN (
+  SELECT
+    uid
+  FROM
+    [hill-climb-racing-2-67064304:materialized_views.lifetime_cheaters_hackers])
